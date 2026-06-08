679f293ab68c26e9a134d3821ad144970fd60271
Davizera16 <davizera1908@gmail.com>
2026-06-08 15:26:19 -0300

    refez o jogo !!!!!!!

---
+++ jogador.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var velocidade_pulo = -600
+
+func _physics_process(delta: float) -> void:
+	
+	velocity += get_gravity() * delta 
+
+	if is_on_floor(): # se está no chão
+		$Imagem.play("default") 
+
+		if Input.is_action_pressed("ui_select"): 
+			velocity.y = velocidade_pulo 
+	else: 
+		$Imagem.play("pular") 
+
+	move_and_slide() 
